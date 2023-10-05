02d3d4a5b82884f547a74ac139551f900081e9d5
chris.miranda <christophermiranda@pursuit.org>
2023-10-05 18:15:54 -0400

    "Finished the lab"

--- index.js
+++ index.js
@@ -9,57 +9,87 @@ const newMember = require("./data/new-member");
  * @param {Object} obj -
  * @return {number} The number of keys in the object
  */
-function numberOfKeys(obj) {}
+function numberOfKeys(obj) {
+  // Initialize var to hold total keys, start at 0
+  let total = 0
+  _.forIn(obj, (key) => {
+    total ++
+  }) 
+  return total
+  // Return total keys
+}
 
 /**
  * Remove the falsy values in a numbers array and return the sum
  * @param {numbers[]} array - An array of numbers that can also contain some falsy values
  * @return {number} The sum of the numbers in an array
  */
-function sumNumbers(array) {}
+function sumNumbers(array) {
+  return _.compact(array).reduce((acc, current)=> acc + current, 0)
+}
 
 /**
  * Convert a two-dimensional array of new member data (each inner array having two values: the first being the key, the second being the value) into an object
  * @param {Array[]} member -
  * @return {number} The sum of the numbers in an array
  */
-function newMemberArrayToObject(member) {}
+function newMemberArrayToObject(member) {
+  return _.fromPairs(member)
+}
 
 /**
  * Return an array of objects that grouped by instructors from the classes array of objects
  * @param {Object[]} collection - an array of yoga class objects
  * @return {Object[]} - the reshaped collection where the classes are grouped by instructor name
  */
-function groupClassByInstructor(collection) {}
+function groupClassByInstructor(collection) {
+  return _.groupBy(collection, "instructor" )
+}
 
 /**
  * Remove the age key from the members array of object
  * @param {Object} collection - an array of member objects
  * @return {number} The array of member objects, each one without the age field
  */
-function omitAgeFromMembers(collection) {}
-
+function omitAgeFromMembers(collection) {
+  let array = []
+  collection.forEach((agelessMember) => {
+    array.push(_.omit(agelessMember, "age"))
+  })
+  return array
+}
 /**
  * Return the count of the number of classes a particular instructor teaches
  * @param {Object[]} collection - An array of yoga class objects
  * @param {String} instructorName - The name of the instructor
  * @return {number} The sum of the numbers in an array
  */
-function countClassesByInstructor(collection, instructor) {}
+function countClassesByInstructor(collection, instructor) {
+  const count = _.countBy(collection, (classes) => instructor === classes.instructor)
+  return count.true || 'There is no instructor by that name.'
+}
+
 
 /**
  * Remove inactive members from the members array
  * @param {Object} collection - an array of member objects
  * @return {number} The array of member objects with only active members
  */
-function removeInactiveMembers(collection) {}
+function removeInactiveMembers(collection) {
+  return _.filter(collection, "currentMember")
+}
 
 /**
  * Get a list of unique class titles and their price
  * @param {Object} collection - an array of yoga class objects
  * @return {number} An array of objects that have a unique title and a price
  */
-function getUniqueClasses(collection) {}
+function getUniqueClasses(collection) {
+  return _.uniqBy(collection, "title").map((singleClass) => ({
+    priceInCents: singleClass.priceInCents,
+    title: singleClass.title
+  }))
+}
 
 /**
  * Get a list of classes organized by title, then by level.
@@ -67,7 +97,11 @@ function getUniqueClasses(collection) {}
  * @param {Object} collection - an array of yoga class objects
  * @return {number} An array of objects that are organized by title then by level. The array should only have the title, instructor, and level fields
  */
-function orderClassesByTitleAndLevel(collection) {}
+function orderClassesByTitleAndLevel(collection){
+  const inOrder = _.orderBy(collection, ['title', 'level'], [ , 'desc'])
+  const finalCollection = _.map(inOrder, ({title, level, instructor}) => ({title, level, instructor}))
+  return finalCollection
+}
 
 module.exports = {
   numberOfKeys,
