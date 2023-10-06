74e7a07ac6bf714d3cbc36afdc3798ca4b0f61a7
Cristian Garcia <crisg@sas.upenn.edu>
2023-10-06 08:43:13 -0400

    up to the first five funcitons completed

--- index.js
+++ index.js
@@ -9,35 +9,55 @@ const newMember = require("./data/new-member");
  * @param {Object} obj -
  * @return {number} The number of keys in the object
  */
-function numberOfKeys(obj) {}
+function numberOfKeys(obj) {
+  const keys = _.keys(obj);
+  return keys.length;
+}
 
 /**
  * Remove the falsy values in a numbers array and return the sum
  * @param {numbers[]} array - An array of numbers that can also contain some falsy values
  * @return {number} The sum of the numbers in an array
  */
-function sumNumbers(array) {}
+function sumNumbers(array) {
+  let sum = 0;
+  _.remove(array, (elt) => !elt);
+  _.forIn(array, (elt) => sum += elt);
+
+  return sum;
+}
 
 /**
- * Convert a two-dimensional array of new member data (each inner array having two values: the first being the key, the second being the value) into an object
+ * Convert a two-dimensional array of new member data 
+ * (each inner array having two values: the first being the key, the
+ *  second being the value) into an object
  * @param {Array[]} member -
  * @return {number} The sum of the numbers in an array
  */
-function newMemberArrayToObject(member) {}
+function newMemberArrayToObject(member) {
+  return _.fromPairs(member);
+}
 
 /**
  * Return an array of objects that grouped by instructors from the classes array of objects
  * @param {Object[]} collection - an array of yoga class objects
  * @return {Object[]} - the reshaped collection where the classes are grouped by instructor name
  */
-function groupClassByInstructor(collection) {}
+function groupClassByInstructor(collection) {
+  // const sortedArr = 
+  return _.groupBy(collection, (elt) => elt.instructor);
+}
 
 /**
  * Remove the age key from the members array of object
  * @param {Object} collection - an array of member objects
  * @return {number} The array of member objects, each one without the age field
  */
-function omitAgeFromMembers(collection) {}
+function omitAgeFromMembers(collection) {
+  return _.map(collection, (elt) => _.omit(elt, 'age'));
+}
+
+// console.log(omitAgeFromMembers(members));
 
 /**
  * Return the count of the number of classes a particular instructor teaches
@@ -45,7 +65,18 @@ function omitAgeFromMembers(collection) {}
  * @param {String} instructorName - The name of the instructor
  * @return {number} The sum of the numbers in an array
  */
-function countClassesByInstructor(collection, instructor) {}
+function countClassesByInstructor(collection, instructor) {
+  const countByInst = _.countBy(collection, (elt) => elt.instructor == instructor);
+  const outCount = countByInst[instructor];
+  
+  if (!outCount) {
+    return 'There is no instructor by that name.';
+  } else {
+    return outCount;
+  }
+}
+
+console.log(countClassesByInstructor(yogaClasses, 'Yin Yoga'));
 
 /**
  * Remove inactive members from the members array
