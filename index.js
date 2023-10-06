900fe9c763a0f5d73b51a7c024cf54753e69d3b5
Carlitos <carlitosdutan@pursuit.org>
2023-10-06 09:56:13 -0400

    Lab Submission, Will Keep Working At It Today

--- index.js
+++ index.js
@@ -9,28 +9,55 @@ const newMember = require("./data/new-member");
  * @param {Object} obj -
  * @return {number} The number of keys in the object
  */
-function numberOfKeys(obj) {}
+function numberOfKeys(obj) {
+  // Initialize var to hold keys, start at 0
+  let total = 0;
+
+  // use forIn
+  _.forIn(obj, (key) => {
+    total++;
+  })
+
+  // Return total keys
+  return total;
+}
 
 /**
  * Remove the falsy values in a numbers array and return the sum
  * @param {numbers[]} array - An array of numbers that can also contain some falsy values
  * @return {number} The sum of the numbers in an array
  */
-function sumNumbers(array) {}
+function sumNumbers(array) {
+  // Use the _.remove to seperate the falsy values
+  let oddNums = _.remove(array, (num) => {
+    return !num;
+  });
+  // Use .reduce to find the sum of all truthy values since we isolated the truthy values
+  let sum = array.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue
+  })
+  // Return the total sum
+  return sum;
+}
 
 /**
  * Convert a two-dimensional array of new member data (each inner array having two values: the first being the key, the second being the value) into an object
  * @param {Array[]} member -
  * @return {number} The sum of the numbers in an array
  */
-function newMemberArrayToObject(member) {}
+function newMemberArrayToObject(member) {
+  // Use _.fromPairs
+  return _.fromPairs(member);
+}
 
 /**
  * Return an array of objects that grouped by instructors from the classes array of objects
  * @param {Object[]} collection - an array of yoga class objects
  * @return {Object[]} - the reshaped collection where the classes are grouped by instructor name
  */
-function groupClassByInstructor(collection) {}
+function groupClassByInstructor(collection) {
+  return _.groupBy(collection, 'instructor');
+}
 
 /**
  * Remove the age key from the members array of object
@@ -45,14 +72,29 @@ function omitAgeFromMembers(collection) {}
  * @param {String} instructorName - The name of the instructor
  * @return {number} The sum of the numbers in an array
  */
-function countClassesByInstructor(collection, instructor) {}
+function countClassesByInstructor(collection, instructor) {
+  let accumulator = 0;
+  //forEach
+
+  _.forEach(collection, (instructorClass) => {
+    if(instructorClass.instructor === instructor){
+      accumulator++;
+    }
+  });
+    if(accumulator === 0){
+      return 'There is no instructor by that name.';
+    }
+    return accumulator;
+}
 
 /**
  * Remove inactive members from the members array
  * @param {Object} collection - an array of member objects
  * @return {number} The array of member objects with only active members
  */
-function removeInactiveMembers(collection) {}
+function removeInactiveMembers(collection) {
+  
+}
 
 /**
  * Get a list of unique class titles and their price
